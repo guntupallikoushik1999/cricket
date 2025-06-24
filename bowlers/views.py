7bca6dba52b5e7d36e51c2e790f5cb8c90f35b48
Guntupalli Koushik Manikanta <guntupallikoushik1999@gmail.com>
2025-06-25 00:47:30 +0530

    Added  custom api filter sort_by, highest_wicket_taker

--- bowlers/views.py
+++ bowlers/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView, Response
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.db.models import Max
 # Create your views here.
 from rest_framework import viewsets
 from .models import Bowler
@@ -16,7 +19,38 @@ class BowlerViewSet(viewsets.ModelViewSet):
         name = self.request.query_params.get('name')
         if name:
             queryset = queryset.filter(name__icontains=name)
-        if age := self.request.query_params.get('age'):
+        if age := self.request.query_params.get('age'): # Filter by age in one liner with if statement check name above for more info
             queryset = queryset.filter(age=age)
+        if country := self.request.query_params.get('country'):
+            queryset = queryset.filter(country__icontains=country)
+        if wickets_taken := self.request.query_params.get('wickets_taken'):
+            queryset = queryset.filter(wickets_taken=wickets_taken)
+        if bowling_average := self.request.query_params.get('bowling_average'):
+            queryset = queryset.filter(bowling_average=bowling_average)
+        if bowling_strikerate := self.request.query_params.get('bowling_strikerate'):
+            queryset = queryset.filter(bowling_strikerate=bowling_strikerate)
+        if economy_rate := self.request.query_params.get('economy_rate'):
+            queryset = queryset.filter(economy_rate=economy_rate)
+        if self.request.query_params.get('sort_by'):
+            sort_by = self.request.query_params.get('sort_by')
+            queryset = queryset.order_by(sort_by)
+        """ 
+        order_by
+        Default DRF sort is ascending order.
+        ?sort_by=name → ascending
+        ?sort_by=-name → descending"""
         return queryset
+    @action(detail=False, methods=['get'], url_path='highest_wicket_taker')
+    #Add a custom GET endpoint called /highest_wicket_taker/ to the BowlerViewSet.
+    # This endpoint will return the bowler(s) with the highest number of wickets taken.
+    def highest_wicket_taker(self, request):
+        max_wickets = Bowler.objects.aggregate(Max('wickets_taken'))['wickets_taken__max']
+        top_bowlers = Bowler.objects.filter(wickets_taken=max_wickets)
+        #above line gets the bowler(s) with the highest number of wickets taken by using aggregate function in Django ORM 
+        #top_bowlers = Bowler.objects.raw('SELECT * FROM bowlers_bowler WHERE wickets_taken > 300')
+        #above line is an example of raw SQL query to get bowlers with more than 300 wickets we can write any SQL query here
+        serializer = self.get_serializer(top_bowlers, many=True)
+        return Response(serializer.data)
+        #return Response(top_bowlers.values('name', 'wickets_taken', 'country'))
+
     
